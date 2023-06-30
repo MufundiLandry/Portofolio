383c7cb5bfba7b10f330e4e55fe6efb963ee8bd0
KulondwaLandry <87100963+kulondwa@users.noreply.github.com>
2023-06-30 05:29:01 -0400

    working on sidebar

---
+++ src/spotify/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "./sidebar/sideBar";
+import "./index.css";
+const Spotify = () => {
+  return (
+    <div className="main">
+      <SideBar />
+    </div>
+  );
+};
+
+export default Spotify;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  important: "#root",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  // important: "#root",
   theme: {
+    screens: {
+      sm: { min: "320px", max: "480px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "481px", max: "768px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "769px", max: "1024px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1025px", max: "1200px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1201px" },
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
-        primary: "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 90%)",
+        primary: "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)",
         secondary: "linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%)",
         footer: "linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)",
         profile: "linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)",
